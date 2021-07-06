1cdad100a65f7ff810d62f39b7aa1c964aefdda9
santoshjammi <santoshjammi@gmail.com>
2021-07-06 12:57:02 +0530

    Create Calculator.py

---
+++ version3/Calculator.py
@@ -0,0 +1,11 @@
+class Calculator:
+
+    def  __init__(self,a,b) -> None:
+        self.a = a
+        self.b = b
+        
+    def add(self):
+        return self.a + self.b
+    
+    def multiply(self):
+        return self.a * self.b
