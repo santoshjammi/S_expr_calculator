c4e1b660c3c7b65f9eb5fa6c649776365886dbc6
santoshjammi <santoshjammi@gmail.com>
2021-07-06 11:31:23 +0530

    made the implementation modular
    made the implementation modular

--- version2/mainfile.py
+++ version2/mainfile.py
@@ -1,45 +1,53 @@
-# Python3 program to Split string into characters
-#"(add (multiply 2 3) 1)"
-def calculate(word):
-    c=word.count('(')
-    mydict={}
-    mywords=[]
-    #print(len(word))
-    mylist=list(word)
-    tempstart=0
-    tempend=0
-    result=0
-    resultadd=0
-    resultmul=1
-    tempstart = word.rindex('(')
-    #print(str(tempstart))
+#Validate the list of keywords
+#For now add and multiply are allowed ones
+def validateKeywords(tempword):
     flag=0
-    tempword=word[tempstart:len(word)]
-    tempend= tempword.index(')')+tempstart
-    #print(word[tempstart:tempend+2])
     allowed_kw=["(add", "(multiply"]
     for kw in allowed_kw:
         if(tempword.split()[0]  == kw):
             flag=1
     if(flag!=1):
         #print(kw, tempword)
-        print("Illegal keyword used")
-        sys.exit()
-    
-    if ('a' in mylist[tempstart + 1] and 'd' in mylist[tempstart + 2] and  'd' in mylist[tempstart + 3]):
-        newstring=word[tempstart+5:tempend]
+        raise Exception("Illegal keyword used. Valid keywords are ")
+
+#Perform the arithmetic operations
+def operateOnNumbers(word, start, end):
+    print(word)
+    #print(str(start) +"  "+end)
+    print(f'{start} {end}')
+    mylist=list(word)
+    resultadd=0
+    resultmul=1
+    if ('a' in mylist[start + 1] and 'd' in mylist[start + 2] and  'd' in mylist[start + 3]):
+        newstring=word[start+5:end]
         newlist=newstring.split()
         for myitem in newlist:
             resultadd=resultadd+int(myitem)
             result=resultadd
-    if ('m' in mylist[tempstart + 1] and 'u' in mylist[tempstart + 2] and  'l' in mylist[tempstart + 3] 
-        and 't' in mylist[tempstart+4] and 'i' in mylist[tempstart+5] and 'p' in mylist[tempstart+6] 
-        and 'l' in mylist[tempstart+7] and 'y' in mylist[tempstart+8] ):
-        newstring=word[tempstart+10:tempend]
+    elif ('m' in mylist[start + 1] and 'u' in mylist[start + 2] and  'l' in mylist[start + 3] 
+        and 't' in mylist[start+4] and 'i' in mylist[start+5] and 'p' in mylist[start+6] 
+        and 'l' in mylist[start+7] and 'y' in mylist[start+8] ):
+        newstring=word[start+10:end]
         newlist=newstring.split()
         for myitem in newlist:
             resultmul=resultmul*int(myitem)
             result=resultmul
+    
+    return result
+
+def calculate(word):
+    c=word.count('(')
+    #print(len(word))
+    tempstart=0
+    tempend=0
+    result=0
+    tempstart = word.rindex('(')
+    #print(str(tempstart))
+    flag=0
+    tempword=word[tempstart:len(word)]
+    tempend= tempword.index(')')+tempstart
+    validateKeywords(tempword)
+    result = operateOnNumbers(word,tempstart,tempend)
     c=c-1
     if(c>0):        
         newword=word[0:tempstart-1] +' ' + str(result) + word[tempend+1:len(word)]
@@ -51,14 +59,8 @@ def calculate(word):
 import os
 import sys
 
-n = len(sys.argv)
-#print("Total arguments passed:", n)
-
-# Arguments passed
-##print("\nName of Python script:", sys.argv[0])
-expression=sys.argv[1]
+#print("\nName of Python script:", sys.argv[0])
+expression=sys.argv[1].lower()
 #expression = "(add (add (add 10 (add 123 245) ) (add 120 121) ) 1)"
 #print(expression)
-
-
 calculate(expression)
